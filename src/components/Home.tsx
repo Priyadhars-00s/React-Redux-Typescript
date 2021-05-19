1ce6cdc3446e05949476ec24283e3ec10059d115
Priyadhars-00s <priyadh.10decoders@gmail.com>
2021-05-19 20:53:23 +0530

    UseEffect error fix

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,97 +1,68 @@
 
-
-import { Redirect } from "react-router-dom";
-
-import axios from "axios";
+import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
 import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import React, { useEffect, useState } from 'react';
 
+import React, { useEffect, useState } from 'react';
+interface IUser {
+  name: [];
+}
+const useStyles = makeStyles({
+  table: {
+    minWidth: 300,
+  },
+});
 export const APIForm: React.FC = () => {
   const [employees, setEmployees] = useState([])
   const [Test, setTest] = useState([])
+  const [user, setUser] = useState<IUser>({name: []});
+useEffect(() => {
 
-
-  // useEffect(() => {
-  //   // Update the document title using the browser API
-  //   document.title = `You clicked ${count} times`;
-  // });
-
-  useEffect(() => {
-    const apiUrl = `http://www.json-generator.com/api/json/get/bOUcubzASW?indent=2`;
-    fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    console.log("data",data.data)
-    const Test= data.data
-    console.log("Test",Test)
-    // setEmployees(data.data)
-  });
-  });
-
-
-  return (
-
+    fetch('http://www.json-generator.com/api/json/get/bOUcubzASW?indent=2')
+    .then(response => response.json())
+    .then(data => 
+      setUser({name: data.data})
+      );
+  }, [])
+const classes = useStyles();
+return (
     <div>
 
-     <TableContainer component={Paper}>
-       {/* <Button onClick={()=>{this.setState({logout: true})}}>logout</Button> */}
-   <Table >
-   <TableHead>
-         <TableRow>
-            <TableCell style={{ width: 160 }} align="center" >
-              ID
-             </TableCell>
-             <TableCell style={{ width: 160 }} align="center" >
-              EMPLOYEE NAME
-             </TableCell>
-             <TableCell style={{ width: 160 }} align="center" >
-              EMPLOYEE AGE
-             </TableCell>
-             <TableCell style={{ width: 160 }} align="center" >
-              EMPLOYEE SALARY
-             </TableCell>
-           </TableRow>
-       </TableHead>
-    <TableBody>
-   {/* { Test.map((data) => ( */}
-          <TableRow key={Test.id}>
-          <TableCell style={{ width: 160 }} align="center">
-            {Test.id}
-          </TableCell>
-          <TableCell >
-            {Test.employee_name}
-          </TableCell>
-          <TableCell style={{ width: 160 }} align="center">
-            {Test.employee_age}
-          </TableCell>
-          <TableCell style={{ width: 160 }} align="center">
-            {Test.employee_salary}
-          </TableCell>
-        </TableRow> 
-       </TableBody> 
+<TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Employee Id</TableCell>
+            <TableCell align="right">Employee Name</TableCell>
+            <TableCell align="right">Employee Salary</TableCell>
+            <TableCell align="right">Employee Age</TableCell>
+          </TableRow>
+        </TableHead>
+<TableBody>
+          {user.name.map((row: any) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell align="right">{row.employee_name}</TableCell>
+              <TableCell align="right">{row.employee_salary}</TableCell>
+              <TableCell align="right">{row.employee_age}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      </TableContainer>
+     
+     </div>
  
-         </Table>
-</TableContainer>
-</div>
-);
-}
+   )
+ }
 
-// const mapStateToProps = (state) => 
 
-// ({
-//   user: state.user.user,
-  
-// });
 
-// const mapDispatchToProps = {
-//   login,
-//   logout,
-// };
-// console.log("Inside mapDispatchToProps=====>",login)
-// export default connect(mapStateToProps, mapDispatchToProps)(Login);
+
